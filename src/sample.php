3e7f695605bc3edf18ac4ac181fe5cd288036341
Akira Koyasu <koyasu@mediba.jp>
2013-08-06 13:44:29 +0900

    add php

---
+++ src/sample.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File doc
+ *
+ * @category Default
+ * @package  Default
+ * @author   Akira Koyasu <koyasu@mediba.jp>
+ * @license  mediba inc. http://www.mediba.jp
+ * @link     none
+ */
+
+/**
+ * class Sample
+ *
+ * @category Default
+ * @package  Default
+ * @author   Akira Koyasu <koyasu@mediba.jp>
+ * @license  mediba inc. http://www.mediba.jp
+ * @link     none
+ */
+class Sample
+{
+
+}
